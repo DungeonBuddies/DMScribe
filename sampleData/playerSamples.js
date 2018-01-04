c3380d4809331f30e87b66c479106b6442b85c49
Jake Lee <jacob.lee715@gmail.com>
2018-01-03 18:00:40 -0600

    Centered order generation and changed number so wouldn't be affected by name size for now. Also added unique keys for all mapping functions

--- sampleData/playerSamples.js
+++ sampleData/playerSamples.js
@@ -16,7 +16,21 @@ const playerSamples = [{
 	"name": "Dude Bronson",
 	"armor_class": 12,
 	"hit_points": 24,
-	"init": 3,
+	"init": 1,
+	"perception": 1,
+	"speed": "30 ft."
+}, {
+	"name": "Jabroni",
+	"armor_class": 16,
+	"hit_points": 18,
+	"init": 7,
+	"perception": 1,
+	"speed": "30 ft."
+}, {
+	"name": "ConCharmi Egopatro",
+	"armor_class": 12,
+	"hit_points": 24,
+	"init": 4,
 	"perception": 1,
 	"speed": "30 ft."
 }, {
