145c3300fb978f8e0d99cbd87a2d3321dcb693e8
Jake Lee <jacob.lee715@gmail.com>
2018-01-06 16:23:23 -0600

    I DID IT! Got the delete functionality working across all tabs. Monsters is almsot completely finished

--- react-client/src/reducers/monstersReducer.js
+++ react-client/src/reducers/monstersReducer.js
@@ -2,7 +2,8 @@ const monstersReducer = (state = [], action) => {
   if (action.type === 'ADD_MONSTER') {
     return [...state, action.payload];
   } else if (action.type === 'DELETE_MONSTER') {
-      return action.payload;
+      const index = state.findIndex(monster => monster.name === action.payload.name);
+      return [...state.slice(0, index), ...state.slice(index + 1)];
   }
   return state;
 };
