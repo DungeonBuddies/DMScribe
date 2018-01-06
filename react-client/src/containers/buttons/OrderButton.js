7e5e671c120b796c0a88abb3db79416b159ad08e
Jake Lee <jacob.lee715@gmail.com>
2018-01-06 17:26:21 -0600

    Added turn order buttons and associated files. Not currently working

--- react-client/src/containers/buttons/OrderButton.js
+++ react-client/src/containers/buttons/OrderButton.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { generateTurnOrder } from '../../actions/index';
 
-export default class OrderButton extends Component {
+class OrderButton extends Component {
   render () {
+    if (this.props.monsters.length === 0) {
+      return null;
+    }
+
   	return(
-  		<div></div>
+  		<div className='orderDiv'>
+        <Button onClick={this.props.generateTurnOrder} content='Generate turn order' />  
+      </div>
   		)
   }
 }
+
+function mapStateToProps (state) {
+  return {
+    monsters: state.monsters
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators({ generateTurnOrder }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderButton);
--- react-client/src/reducers/index.js
+++ react-client/src/reducers/index.js
@@ -3,12 +3,14 @@ import monstersReducer from './monstersReducer';
 import playersReducer from './playersReducer';
 import monsterUrlsReducer from './monsterUrlsReducer';
 import currentTabReducer from './currentTabReducer';
+import turnOrderReducer from './turnOrderReducer';
 
 const reducer = combineReducers({
   monsters: monstersReducer,
   players: playersReducer,
   monsterUrls: monsterUrlsReducer,
-  currentTab: currentTabReducer
+  currentTab: currentTabReducer,
+  turnOrder: turnOrderReducer
 });
 
 export default reducer;
