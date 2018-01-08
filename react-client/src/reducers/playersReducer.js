d1927f41e85bbf9fc8e324e1ba1e0b2ea700065c
Jake Lee <jacob.lee715@gmail.com>
2018-01-08 15:02:48 -0600

    Added starter sample data to populate arena

--- react-client/src/reducers/playersReducer.js
+++ react-client/src/reducers/playersReducer.js
@@ -1,4 +1,32 @@
-const playersReducer = (state = [], action) => {
+const sample = [
+  {
+   name: 'Jake',
+   class: 'Warrior',
+   armor_class: 17,
+   hit_points: 60,
+   init: 1,
+   perception: 10,
+   speed: 30
+  }, {
+   name: 'Brooks',
+   class: 'Bard',
+   armor_class: 15,
+   hit_points: 55,
+   init: 1,
+   perception: 15,
+   speed: 30
+  }, {
+   name: 'Spencer',
+   class: 'Wizard',
+   armor_class: 14,
+   hit_points: 50,
+   init: 2,
+   perception: 15,
+   speed: 25
+  }
+]
+
+const playersReducer = (state = sample, action) => {
   if (action.type === 'ADD_PLAYER') {
     return [...state, action.payload];
   }
