33830cffd125bdcaa674815c0de6e7f58df40b51
Jake Lee <jacob.lee715@gmail.com>
2018-01-08 13:31:35 -0600

    Added clear all monsters button

--- react-client/src/reducers/monstersReducer.js
+++ react-client/src/reducers/monstersReducer.js
@@ -5,16 +5,18 @@ const monstersReducer = (state = [], action) => {
     //   return monster; 
     // })
     // console.log(updated)
-    action.payload.id = action.payload._id.concat(state.length);
-    return [...state, action.payload];
+      action.payload.id = action.payload._id.concat(state.length);
+      return [...state, action.payload];
 
   } else if (action.type === 'DELETE_MONSTER') {
-    const index = state.findIndex(monster => monster.id === action.payload.id);
-    return [...state.slice(0, index), ...state.slice(index + 1)];
+      const index = state.findIndex(monster => monster.id === action.payload.id);
+      return [...state.slice(0, index), ...state.slice(index + 1)];
 
   } else if (action.type === 'ADD_MONSTER_IMG') {
-    const index = state.findIndex(monster => monster.id === action.payload.id);
-    return [...state.slice(0, index), {...state[index], image: action.payload.url}, ...state.slice(index + 1)];
+      const index = state.findIndex(monster => monster.id === action.payload.id);
+      return [...state.slice(0, index), {...state[index], image: action.payload.url}, ...state.slice(index + 1)];
+  } else if (action.type === 'CLEAR_MONSTERS') {
+      return action.payload;
   }
   return state;
 };
