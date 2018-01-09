f68cf8961b0e4d905043ec30b3fd41c7714508c6
Spencer Vaterlaus <spencervaterlaus@gmail.com>
2018-01-09 11:50:52 -0600

    Set turn order on addMonster + cleanup

--- react-client/src/actions/index.js
+++ react-client/src/actions/index.js
@@ -27,19 +27,14 @@ export const addMonster = (url, checked) => {
   .then(res => {
     const monster = res.data;
     monster.init = Math.floor((monster.dexterity - 10) / 2);
-    // var monsters = [];
-    // for (var i = 0; i < number; i++) {
-    //   monsters.push(monster);
-    // };
     if (checked) {
       monster.order = monster.init + (Math.floor(Math.random() * Math.floor(20)));
-      // monsters.map((monster) => {
-      //   return monster.init = Math.floor((monster.dexterity - 10) / 2) + (Math.floor(Math.random() * Math.floor(20)));
-      // });
       store.dispatch({type: ADD_MONSTER, payload: monster});
     } else {
       store.dispatch({type: ADD_MONSTER, payload: monster});
     }
+
+    store.dispatch(generateTurnOrder());
 
     fetchMonsterImg(monster.name)
     .then(url => {
