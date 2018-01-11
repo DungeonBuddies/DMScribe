f6cff89dd6bd257813b5175b2c813791390d75d9
Jake Lee <jacob.lee715@gmail.com>
2018-01-11 17:41:53 -0600

    LANDING PAGE IS BEAUTIFUL

--- react-client/src/containers/LandingPage.js
+++ react-client/src/containers/LandingPage.js
@@ -26,46 +26,45 @@ class LandingPage extends Component {
 
 	render() {
     const resizeMode = 'center';
-    return (
 
-      <div className="ui grid">
-        <Menu fixed='top' size='large'>
-          <Container>
-            <Menu.Menu position='right'>
-              <Menu.Item className='item'>
-                <Button as='a'>Log in</Button>
-              </Menu.Item>
-              <Menu.Item>
-                <Button as='a' primary>Sign Up</Button>
-              </Menu.Item>
-            </Menu.Menu>
-          </Container>
-        </Menu>
-
-  			<Container>
-          <Header
-            as='h1'
-            content='DM-Scribe'
-            inverted
-            style={{ backgroundColor: 'transparent', fontSize: '4em', fontWeight: 'normal', color:'#1b1c1d', marginBottom: 0, marginTop: '3em' }}
-          />
-          <Header
-            as='h2'
-            content='The handy tool for the unorganized DM'
-            inverted
-            style={{ backgroundColor: 'transparent', fontSize: '1.7em', color:'#1b1c1d', fontWeight: 'normal' }}
-          />
-          <Button primary size='huge' onClick={this.props.clickToArena}>
-            Roll for Initiative!
-            <Icon name='right arrow'/>
-          </Button>
-          <img id="landing-image" className="ui large right floated image" src="https://png.icons8.com/ultraviolet/540/icosahedron.png"/>
-        </Container>
-      </div>
-		)
     if (this.state.page === 'landing') {
   		return (
+
         <div>
+          <div className="ui grid">
+            <Menu fixed='top' size='large'>
+              <Container>
+                <Menu.Menu position='right'>
+                  <Menu.Item className='item'>
+                    <Button as='a'>Log in</Button>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <Button as='a' primary>Sign Up</Button>
+                  </Menu.Item>
+                </Menu.Menu>
+              </Container>
+            </Menu>
+
+            <Container>
+              <Header
+                as='h1'
+                content='DM-Scribe'
+                inverted
+                style={{ backgroundColor: 'transparent', fontSize: '4em', fontWeight: 'normal', color:'#1b1c1d', marginBottom: 0, marginTop: '3em' }}
+              />
+              <Header
+                as='h2'
+                content='The handy tool for the unorganized DM'
+                inverted
+                style={{ backgroundColor: 'transparent', fontSize: '1.7em', color:'#1b1c1d', fontWeight: 'normal' }}
+              />
+              <Button primary size='huge' onClick={this.props.clickToArena}>
+                Roll for Initiative!
+                <Icon name='right arrow'/>
+              </Button>
+              <img id="landing-image" className="ui large right floated image" src="https://png.icons8.com/ultraviolet/540/icosahedron.png"/>
+            </Container>
+          </div>
           <Menu fixed='top' size='large'>
             <Container>
               <Menu.Menu position='right'>
