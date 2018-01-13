453aef2ea60d956cfc1de6ef5544809207508c5b
Spencer Vaterlaus <spencervaterlaus@gmail.com>
2018-01-13 15:19:58 -0600

    Fix dependency error

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,8 +1,7 @@
 var mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/groups');
 // var uri = `mongodb://${cred.dbUsername}:${cred.dbPassword}@ds255787.mlab.com:55787/heroku_jhf97sfb`;
-
-var cred = require('./dbCredentials');
+// var cred = require('./dbCredentials');
 
 mongoose.connect(MONGOLAB_RED_URI);
 var db = mongoose.connection;
